a51e1d01a25bf2eb125a504e41a1f254fb55abe1
BRAULIO\braul <braulio.castro11@gmail.com>
2019-04-19 22:18:23 -0600

    [nuevo] conversión del mensaje a QR con protocolo

--- codigo_fuente/QR_code_gen/generador_codigos.py
+++ codigo_fuente/QR_code_gen/generador_codigos.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import hashlib
 import pyqrcode
 import os
 from pyqrcode import QRCode
@@ -8,9 +9,16 @@ from pyqrcode import QRCode
 class gen_codQR:
 
 	msj_dir = "D:\\braul\\Documents\\tec\\2019-I\\Redes\\Proyectos\\Proyecto1\\codigo_fuente\\Pruebas\\"
+	msj_tam = 87
+	sha1 = ""
+	qr_0 = "1 0 0 "
 
-	def __init__(self):
-		pass
+	def __init__(self, mac_em, mac_re, ip_em, ip_re):
+		self.mac_em = mac_em
+		self.mac_re = mac_re
+		self.ip_em = ip_em
+		self.ip_re = ip_re
+
 
 	# Entrada: (nombre de archivo)
 	# Salida: contenido del archivo
@@ -18,29 +26,57 @@ class gen_codQR:
 		arch =  open(nom_arch, 'rb')
 		cont = arch.read()
 		arch.close()
+		self.sha1 = self.codificar(cont)
+		self.sha1 += "#"
 		return cont
+
+	def codificar(self, cont):
+		sha1 = hashlib.sha1(cont)
+		val_hex = sha1.hexdigest()
+		val_bytes = bytearray(sha1.digest())
+		val_int = int(val_hex, 16)
+		val_int = str(val_hex)
+		return val_int
 
 	# Entrada: (texto, tamaño total, tamaño de cada porción)
 	# Salida: (array con texto dividido en porciones tam)
-	def dividir_archivo(self, cont, bytes_arch, tam):
-		return [cont[i:i+tam] for i in range(0, bytes_arch, tam)]
+	def dividir_archivo(self, cont, bytes_arch):
+		return [cont[i:i+self.msj_tam] for i in range(0, bytes_arch, self.msj_tam)]
 
 	# Primero hay que definir el protocolo
-	def convertir(self):
-		url = pyqrcode.create(file_content)
-		url.svg("myqr.svg", scale = 8)
+	def convertir(self, msj, tam):
+		for i in range(0, tam):
+			qr = pyqrcode.create(msj[i])
+			qr.svg(str(i) + "myqr.svg", scale = 8)
+
+	def protocolo(self, msj):
+		self.qr_0 += self.sha1
+		self.qr_0 += self.mac_em + " "
+		self.qr_0 += self.mac_re + " "
+		self.qr_0 += self.ip_em + " "
+		self.qr_0 += self.ip_re
+		largo_msj = len(msj)
+		msj = [("0 " + "0 " + str(i) + " " + self.sha1 + msj[i]) for i in range(0, largo_msj)]
+		msj = [self.qr_0] + msj
+		return msj
+
 
 	# Entrada: (nombre de archivo, tamaño de cada porción de texto)
-	def generar(self, nom_arch, tam):
+	def generar(self, nom_arch):
 		cont = self.leer_archivo(self.msj_dir + nom_arch)
 		bytes_arch = len(cont)
-		arch_div = self.dividir_archivo(cont, bytes_arch, tam)
-		print arch_div
+		msj = self.dividir_archivo(cont, bytes_arch)
+		msj2 = self.protocolo(msj)
+		self.convertir(msj2, len(msj2))
 
 
 def main():
-	generador = gen_codQR()
-	generador.generar("texto.txt", 15)
+	mac_em_prueba = "0017FC340000"
+	mac_re_prueba = "0017FC250000"
+	ip_em_prueba = "172.16.254.1"
+	ip_re_prueba = "192.168.1.24"
+	generador = gen_codQR(mac_em_prueba,mac_re_prueba,ip_em_prueba,ip_re_prueba)
+	generador.generar("texto3.txt")
 
 if __name__ == '__main__':
 	main()
--- codigo_fuente/QR_code_gen/pruebas.py
+++ codigo_fuente/QR_code_gen/pruebas.py
@@ -5,7 +5,7 @@ from pyqrcode import QRCode
 
 
 pruebas_dir = "D:\\braul\\Documents\\tec\\2019-I\\Redes\\Proyectos\\Proyecto1\\codigo_fuente\\Pruebas\\"
-prueba = pruebas_dir + "texto.txt"
+prueba = pruebas_dir + "imagen.png"
 
 stat = os.path.getsize(prueba)
 print stat
@@ -22,3 +22,22 @@ print file_content
 url = pyqrcode.create(file_content)
 
 url.svg("myqr.svg", scale = 8)
+
+# I - 2 byte
+# F - 2 byte
+# # - 8 bytes
+# xxxxxxxxxxxx - 13 bytes
+# xxxxxxxxxxxx - 13 bytes
+# xxx.xxx.xxx.xxx - 16 bytes
+# xxx.xxx.xxx.xxx - 16 bytes
+
+# protocolo - 70 bytes
+
+# mensaje - 58 bytes
+
+# total - 128 bytes
+
+# 1369fa9e0b933431ba9debea60cf9834a3617202
+# d3356b5961aacbbc9e6d3e41da1e59935f88b50c
+
+# 40
